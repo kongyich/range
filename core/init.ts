ade1340e6cb01b2c375a6c52028ecc58bae1d4c6
“keyie” <570588655@qq.com>
2022-04-23 11:19:26 +0800

    init

---
+++ core/init.ts
@@ -0,0 +1,6 @@
+
+export const init = function() {
+    console.log('hello range!');
+}
+
+
---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript';
+import pkg from './package.json';
+
+export default {
+  input: 'core/init.ts', // 打包入口
+  output: { // 打包出口
+	file: pkg.browser, // 最终打包出来的文件路径和文件名，这里是在package.json的browser: 'dist/index.js'字段中配置的
+    format: 'umd', // umd是兼容amd/cjs/iife的通用打包格式，适合浏览器
+    name: 'file'
+  },
+  plugins: [ // 打包插件
+    resolve(), // 查找和打包node_modules中的第三方模块
+    commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
+    typescript() // 解析TypeScript
+  ]
+};
